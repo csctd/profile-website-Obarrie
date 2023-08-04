1d1be95ff219a84e24e7d40e488d5ed352b07c72
Obarrie <113312631+Obarrie@users.noreply.github.com>
2023-08-04 09:22:37 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 # -- Project information -----------------------------------------------------
 
-project = 'Site Title'
-copyright = '2022, Your name'
-author = 'Your Name'
+project = 'Profile Website'
+copyright = '2022, Osman'
+author = 'Osman Barrie'
 
 
 
